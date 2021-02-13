ca68dabc2f69ed18e2daf64ddf11c85017bbf91f
Changsoo Kim <tiratano@naver.com>
2021-02-14 01:28:20 +0900

    - add youtube capture feature

--- Form1.cs
+++ Form1.cs
@@ -21,7 +21,11 @@ namespace coursera_capture
 {
     public partial class Form1 : Form
     {
-        public static Size courseraSize = new Size(940, 585);
+        public static Point courseraLeftTop = new Point(442, 225);
+        public static Size courseraSize = new Size(940, 585);        
+
+        public static Point youTubeLeftTop = new Point(310, 160);
+        public static Size youTubeSize = new Size(965, 620);
         public static String startupName = "CourseraUtils";
         KeyboardHook hook = new KeyboardHook();
 
@@ -39,12 +43,23 @@ namespace coursera_capture
             hook.KeyPressed += new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
             // register the control + alt + F12 combination as hot key.
             //hook.RegisterHotKey(coursera_capture.ModifierKeys.Control | coursera_capture.ModifierKeys.Alt, Keys.F12);
+
+            // coursera 캡쳐
             hook.RegisterHotKey(coursera_capture.ModifierKeys.Control, Keys.D1);
+
+            // youtube 캡쳐
+            hook.RegisterHotKey(coursera_capture.ModifierKeys.Control, Keys.D2);
         }
 
         void hook_KeyPressed(object sender, KeyPressedEventArgs e)
         {
-            captureCourseraScreen();
+            if (e.Key == Keys.D1)
+            {
+                captureScreen(courseraLeftTop.X, courseraLeftTop.Y, courseraSize.Width, courseraSize.Height);
+            } else if (e.Key == Keys.D2)
+            {
+                captureScreen(youTubeLeftTop.X, youTubeLeftTop.Y, youTubeSize.Width, youTubeSize.Height);
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -54,7 +69,12 @@ namespace coursera_capture
 
         private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            captureCourseraScreen();
+            
+        }
+
+        private void captureCourseraMenuItem_Click(object sender, EventArgs e)
+        {
+            captureScreen(courseraLeftTop.X, courseraLeftTop.Y, courseraSize.Width, courseraSize.Height);
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -63,12 +83,7 @@ namespace coursera_capture
             Application.Exit();
         }
 
-        private void captureCourseraMenuItem_Click(object sender, EventArgs e)
-        {
-            captureCourseraScreen();
-        }
-
-        public void captureCourseraScreen()
+        public void captureScreen(int x, int y, int width, int height)
         {
             // Capture
             //작업 표시줄을 제외한 영역 크기   
@@ -82,7 +97,7 @@ namespace coursera_capture
             int screenHeight = SystemInformation.VirtualScreen.Height;
 
             //Bitmap 객체 생성   
-            Bitmap bmp = new Bitmap(Form1.courseraSize.Width, Form1.courseraSize.Height);
+            Bitmap bmp = new Bitmap(width, height);
 
             //Graphics 객체 생성   
             Graphics g = Graphics.FromImage(bmp);
@@ -90,7 +105,7 @@ namespace coursera_capture
             //Graphics 객체의 CopyFromScreen()메서드로 bitmap 객체에 Screen을 캡처하여 저장   
 
             //g.CopyFromScreen(screenLeft, screenTop, 0, 0, bmp.Size);
-            g.CopyFromScreen(screenLeft + 442, screenTop + 225, 0, 0, Form1.courseraSize);
+            g.CopyFromScreen(screenLeft + x, screenTop + y, 0, 0, new Size(width, height));
 
             Clipboard.SetImage((Image)bmp);
         }
